0e638f60c3d77e03d7b9c1cabc0ba981b343bfd0
Scott Gottreu <sgottreu@gmail.com>
2013-06-19 21:04:08 -0500

    Trying to implement shortest distance equation.

--- app/models/City.js
+++ app/models/City.js
@@ -1,40 +1,197 @@
-var cities = new Array();
+var citiesByName = new Array();
 
-cities['Atlanta'] = { id: 1};
-cities['Boston'] = { id: 2};
-cities['Calgary'] = { id: 3};
-cities['Charleston'] = { id: 4};
-cities['Chicago'] = { id: 5};
-cities['Dallas'] = { id: 6};
-cities['Denver'] = { id: 7};
-cities['Duluth'] = { id: 8};
-cities['El Paso'] = { id: 9};
-cities['Helena'] = { id: 10};
-cities['Houston'] = { id: 11};
-cities['Kansas City'] = { id: 12};
-cities['Las Vegas'] = { id: 13};
-cities['Little Rock'] = { id: 14};
-cities['Los Angeles'] = { id: 15};
-cities['Miami'] = { id: 16};
-cities['Montreal'] = { id: 17};
-cities['Nashville'] = { id: 18};
-cities['New Orleans'] = { id: 19};
-cities['New York City'] = { id: 20};
-cities['Oklahoma City'] = { id: 21};
-cities['Omaha'] = { id: 22};
-cities['Phoenix'] = { id: 23};
-cities['Pittsburgh'] = { id: 24};
-cities['Portland'] = { id: 25};
-cities['Raleigh'] = { id: 26};
-cities['Salt Lake City'] = { id: 27};
-cities['San Francisco'] = { id: 28};
-cities['Santa Fe'] = { id: 29};
-cities['Sault Ste Marie'] = { id: 30};
-cities['Seattle'] = { id: 31};
-cities['St Louis'] = { id: 32};
-cities['Toronto'] = { id: 33};
-cities['Vancouver'] = { id: 34};
-cities['Washington, DC'] = { id: 35};
-cities['Winnipeg'] = { id: 36};
+citiesByName['Atlanta'] = { id: 1};
+citiesByName['Boston'] = { id: 2};
+citiesByName['Calgary'] = { id: 3};
+citiesByName['Charleston'] = { id: 4};
+citiesByName['Chicago'] = { id: 5};
+citiesByName['Dallas'] = { id: 6};
+citiesByName['Denver'] = { id: 7};
+citiesByName['Duluth'] = { id: 8};
+citiesByName['El Paso'] = { id: 9};
+citiesByName['Helena'] = { id: 10};
+citiesByName['Houston'] = { id: 11};
+citiesByName['Kansas City'] = { id: 12};
+citiesByName['Las Vegas'] = { id: 13};
+citiesByName['Little Rock'] = { id: 14};
+citiesByName['Los Angeles'] = { id: 15};
+citiesByName['Miami'] = { id: 16};
+citiesByName['Montreal'] = { id: 17};
+citiesByName['Nashville'] = { id: 18};
+citiesByName['New Orleans'] = { id: 19};
+citiesByName['New York City'] = { id: 20};
+citiesByName['Oklahoma City'] = { id: 21};
+citiesByName['Omaha'] = { id: 22};
+citiesByName['Phoenix'] = { id: 23};
+citiesByName['Pittsburgh'] = { id: 24};
+citiesByName['Portland'] = { id: 25};
+citiesByName['Raleigh'] = { id: 26};
+citiesByName['Salt Lake City'] = { id: 27};
+citiesByName['San Francisco'] = { id: 28};
+citiesByName['Santa Fe'] = { id: 29};
+citiesByName['Sault Ste Marie'] = { id: 30};
+citiesByName['Seattle'] = { id: 31};
+citiesByName['St Louis'] = { id: 32};
+citiesByName['Toronto'] = { id: 33};
+citiesByName['Vancouver'] = { id: 34};
+citiesByName['Washington, DC'] = { id: 35};
+citiesByName['Winnipeg'] = { id: 36};
 
-exports.getCities = function() { return cities; }
+exports.getCitiesByName = function() { return citiesByName; }
+
+var citiesById = new Array();
+citiesById[1] ='Atlanta';
+citiesById[2] ='Boston';
+citiesById[3] ='Calgary';
+citiesById[4] ='Charleston';
+citiesById[5] ='Chicago';
+citiesById[6] ='Dallas';
+citiesById[7] ='Denver';
+citiesById[8] ='Duluth';
+citiesById[9] ='El Paso';
+citiesById[10] ='Helena';
+citiesById[11] ='Houston';
+citiesById[12] ='Kansas City';
+citiesById[13] ='Las Vegas';
+citiesById[14] ='Little Rock';
+citiesById[15] ='Los Angeles';
+citiesById[16] ='Miami';
+citiesById[17] ='Montreal';
+citiesById[18] ='Nashville';
+citiesById[19] ='New Orleans';
+citiesById[20] ='New York City';
+citiesById[21] ='Oklahoma City';
+citiesById[22] ='Omaha';
+citiesById[23] ='Phoenix';
+citiesById[24] ='Pittsburgh';
+citiesById[25] ='Portland';
+citiesById[26] ='Raleigh';
+citiesById[27] ='Salt Lake City';
+citiesById[28] ='San Francisco';
+citiesById[29] ='Santa Fe';
+citiesById[30] ='Sault Ste Marie';
+citiesById[31] ='Seattle';
+citiesById[32] ='St Louis';
+citiesById[33] ='Toronto';
+citiesById[34] ='Vancouver';
+citiesById[35] ='Washington, DC';
+citiesById[36] ='Winnipeg';
+
+exports.getCitiesById = function() { return citiesById; }
+
+
+var paths = { Atlanta: 
+   { Charleston: 2,
+     Miami: 5,
+     Nashville: 1,
+     'New Orleans': 4,
+     Raleigh: 2 },
+  Boston: { Montreal: 2, 'New York City': 2 },
+  Calgary: { Helena: 4, Seattle: 4, Vancouver: 3, Winnipeg: 6 },
+  Charleston: { Atlanta: 2, Miami: 4, Raleigh: 2 },
+  Chicago: { Duluth: 3, Omaha: 4, Pittsburgh: 3, 'St Louis': 2, Toronto: 4 },
+  Dallas: { 'El Paso': 4, Houston: 1, 'Little Rock': 2, 'Oklahoma City': 2 },
+  Denver: 
+   { Helena: 4,
+     'Kansas City': 4,
+     'Oklahoma City': 4,
+     Omaha: 4,
+     Phoenix: 5,
+     'Salt Lake City': 3,
+     'Santa Fe': 2 },
+  Duluth: 
+   { Chicago: 3,
+     Helena: 6,
+     Omaha: 2,
+     'Sault Ste Marie': 3,
+     Toronto: 6,
+     Winnipeg: 4 },
+  'El Paso': 
+   { Dallas: 4,
+     Houston: 6,
+     'Los Angeles': 5,
+     'Oklahoma City': 5,
+     Phoenix: 3,
+     'Santa Fe': 2 },
+  Helena: 
+   { Calgary: 4,
+     Denver: 4,
+     Omaha: 5,
+     'Salt Lake City': 3,
+     Seattle: 6,
+     Winnipeg: 4 },
+  Houston: { Dallas: 1, 'El Paso': 6, 'New Orleans': 3 },
+  'Kansas City': { Denver: 4, 'Oklahoma City': 2, Omaha: 1, 'St Louis': 2 },
+  'Las Vegas': { 'Los Angeles': 2, 'Salt Lake City': 3 },
+  'Little Rock': 
+   { Dallas: 2,
+     Nashville: 3,
+     'New Orleans': 3,
+     'Oklahoma City': 2,
+     'St Louis': 2 },
+  'Los Angeles': { 'El Paso': 6, 'Las Vegas': 2, Phoenix: 3, 'San Francisco': 3 },
+  Miami: { Atlanta: 5, Charleston: 4, 'New Orleans': 6 },
+  Montreal: 
+   { Boston: 2,
+     'New York City': 3,
+     'Sault Ste Marie': 5,
+     Toronto: 3 },
+  Nashville: 
+   { Atlanta: 1,
+     'Little Rock': 3,
+     Pittsburgh: 4,
+     Raleigh: 3,
+     'St Louis': 2 },
+  'New Orleans': { Atlanta: 4, Houston: 2, 'Little Rock': 3, Miami: 6 },
+  'New York City': { Boston: 2, Montreal: 3, Pittsburgh: 2, 'Washington, DC': 2 },
+  'Oklahoma City': 
+   { Dallas: 2,
+     Denver: 4,
+     'Kansas City': 2,
+     'Little Rock': 2,
+     'Santa Fe': 3 },
+  Omaha: { Chicago: 4, Denver: 4, Duluth: 2, Helena: 5, 'Kansas City': 1 },
+  Phoenix: { Denver: 5, 'El Paso': 3, 'Los Angeles': 3, 'Santa Fe': 3 },
+  Pittsburgh: 
+   { Chicago: 3,
+     Nashville: 4,
+     'New York City': 2,
+     Raleigh: 2,
+     'St Louis': 5,
+     Toronto: 2,
+     'Washington, DC': 2 },
+  Portland: { 'Salt Lake City': 6, 'San Francisco': 5, Seattle: 1 },
+  Raleigh: 
+   { Atlanta: 2,
+     Charleston: 2,
+     Nashville: 3,
+     Pittsburgh: 2,
+     'Washington, DC': 2 },
+  'Salt Lake City': 
+   { Denver: 3,
+     Helena: 3,
+     'Las Vegas': 3,
+     Portland: 6,
+     'San Francisco': 5 },
+  'San Francisco': { 'Los Angeles': 3, Portland: 5, 'Salt Lake City': 5 },
+  'Santa Fe': { Denver: 2, 'El Paso': 2, 'Oklahoma City': 3, Phoenix: 3 },
+  'Sault Ste Marie': { Duluth: 3, Montreal: 5, Toronto: 2, Winnipeg: 6 },
+  Seattle: { Calgary: 4, Helena: 6, Portland: 1, Vancouver: 1 },
+  'St Louis': 
+   { Chicago: 2,
+     'Kansas City': 2,
+     'Little Rock': 2,
+     Nashville: 2,
+     Pittsburgh: 5 },
+  Toronto: 
+   { Chicago: 4,
+     Duluth: 6,
+     Montreal: 3,
+     Pittsburgh: 2,
+     'Sault Ste Marie': 2 },
+  Vancouver: { Calgary: 3, Seattle: 1 },
+  'Washington, DC': { 'New York City': 2, Pittsburgh: 2, Raleigh: 2 },
+  Winnipeg: { Calgary: 6, Duluth: 4, Helena: 4, 'Sault Ste Marie': 6 } };
+  
+  
+exports.getPaths = function() { return paths; }
--- routes/route.js
+++ routes/route.js
@@ -10,15 +10,72 @@ var mongoose = require('mongoose')
 
 var Route = require('../app/models/Route');
 var City = require('../app/models/City');
+//var Graph = require('../app/models/Graph');
 
 //var objTrain = mongoose.model('Train', Schema);
 
 
 exports.list = function(req, res){
 
-    console.log(City.getCities());
+    var cities = City.getCitiesById();
+    var routes = Route.getRoutes();
+/*
+    var tmp = new Object;
+
+    for(x=0;x<routes.length;x++)
+
+    {
+        tmp[cities[x+1]] = new Object;
     
-    res.render('route_list', { routes: Route.getRoutes() } );
+    
+        for(y=0;y<routes.length;y++)
+        {
+            if(routes[x][y] !== Infinity && routes[x][y] !== 0) {
+                tmp[cities[x+1]][cities[y+1]] = routes[x][y];
+            }
+            if (y == 35 && routes[x][y] === undefined) { console.log(x);}
+        }
+    
+    }
+    console.log(tmp);
+*/
+
+    var paths = City.getPaths();
+    
+    //console.log(paths);    
+    /*
+var graph = new Graph(paths);
+
+    var current_route = graph.findShortestPath('St Louis', 'Seatlle');
+    
+    console.log(graph.findShortestPath('Portland', 'Seatlle'));
+*/
+
+
+    var start = 1;
+    var end = 6;
+    
+    // Compute the shortest paths from vertex number 1 to each other vertex
+    // in the graph.
+    var shortestPathInfo = shortestPath(routes, 36, 0);
+ console.log(  shortestPathInfo); 
+    // Get the shortest path from vertex 1 to vertex 6.
+    var path0to6 = constructPath(shortestPathInfo, end);
+    
+    console.log('start: '+cities[start+1]);
+    
+    for(x=0;x<path0to6.length;x++)
+    {
+        if(x<path0to6.length-1) {
+            console.log('next: '+cities[x+1]);
+        } else {
+            console.log('end: '+cities[x+1]);
+        }
+    }
+    
+    console.log(path0to6);
+    
+    res.render('route_list', { routes: routes } );
 
    
 
@@ -26,3 +83,211 @@ exports.list = function(req, res){
 };
 
 
+var Graph = (function (undefined) {
+
+	var extractKeys = function (obj) {
+		var keys = [], key;
+		for (key in obj) {
+		    Object.prototype.hasOwnProperty.call(obj,key) && keys.push(key);
+		}
+		return keys;
+	}
+
+	var sorter = function (a, b) {
+		return parseFloat (a) - parseFloat (b);
+	}
+
+	var findPaths = function (map, start, end, infinity) {
+		infinity = infinity || Infinity;
+
+		var costs = {},
+		    open = {'0': [start]},
+		    predecessors = {},
+		    keys;
+
+		var addToOpen = function (cost, vertex) {
+			var key = "" + cost;
+			if (!open[key]) open[key] = [];
+			open[key].push(vertex);
+		}
+
+		costs[start] = 0;
+
+		while (open) {
+			if(!(keys = extractKeys(open)).length) break;
+
+			keys.sort(sorter);
+
+			var key = keys[0],
+			    bucket = open[key],
+			    node = bucket.shift(),
+			    currentCost = parseFloat(key),
+			    adjacentNodes = map[node] || {};
+
+			if (!bucket.length) delete open[key];
+
+			for (var vertex in adjacentNodes) {
+			    if (Object.prototype.hasOwnProperty.call(adjacentNodes, vertex)) {
+					var cost = adjacentNodes[vertex],
+					    totalCost = cost + currentCost,
+					    vertexCost = costs[vertex];
+
+					if ((vertexCost === undefined) || (vertexCost > totalCost)) {
+						costs[vertex] = totalCost;
+						addToOpen(totalCost, vertex);
+						predecessors[vertex] = node;
+					}
+				}
+			}
+		}
+
+		if (costs[end] === undefined) {
+			return null;
+		} else {
+			return predecessors;
+		}
+
+	}
+
+	var extractShortest = function (predecessors, end) {
+		var nodes = [],
+		    u = end;
+
+		while (u) {
+			nodes.push(u);
+			predecessor = predecessors[u];
+			u = predecessors[u];
+		}
+
+		nodes.reverse();
+		return nodes;
+	}
+
+	var findShortestPath = function (map, nodes) {
+		var start = nodes.shift(),
+		    end,
+		    predecessors,
+		    path = [],
+		    shortest;
+
+		while (nodes.length) {
+			end = nodes.shift();
+			predecessors = findPaths(map, start, end);
+
+			if (predecessors) {
+				shortest = extractShortest(predecessors, end);
+				if (nodes.length) {
+					path.push.apply(path, shortest.slice(0, -1));
+				} else {
+					return path.concat(shortest);
+				}
+			} else {
+				return null;
+			}
+
+			start = end;
+		}
+	}
+
+	var toArray = function (list, offset) {
+		try {
+			return Array.prototype.slice.call(list, offset);
+		} catch (e) {
+			var a = [];
+			for (var i = offset || 0, l = list.length; i < l; ++i) {
+				a.push(list[i]);
+			}
+			return a;
+		}
+	}
+
+	var Graph = function (map) {
+		this.map = map;
+	}
+
+	Graph.prototype.findShortestPath = function (start, end) {
+		if (Object.prototype.toString.call(start) === '[object Array]') {
+			return findShortestPath(this.map, start);
+		} else if (arguments.length === 2) {
+			return findShortestPath(this.map, [start, end]);
+		} else {
+			return findShortestPath(this.map, toArray(arguments));
+		}
+	}
+
+	Graph.findShortestPath = function (map, start, end) {
+		if (Object.prototype.toString.call(start) === '[object Array]') {
+			return findShortestPath(map, start);
+		} else if (arguments.length === 3) {
+			return findShortestPath(map, [start, end]);
+		} else {
+			return findShortestPath(map, toArray(arguments, 1));
+		}
+	}
+
+	return Graph;
+
+})();
+
+
+
+
+
+/*
+ * dijkstra.js
+ *
+ * Dijkstra's single source shortest path algorithm in JavaScript.
+ *
+ * Cameron McCormack <cam (at) mcc.id.au>
+ *
+ * Permission is hereby granted to use, copy, modify and distribute this
+ * code for any purpose, without fee.
+ *
+ * Initial version: October 21, 2004
+ */
+
+function shortestPath(edges, numVertices, startVertex) {
+  var done = new Array(numVertices);
+  done[startVertex] = true;
+  var pathLengths = new Array(numVertices);
+  var predecessors = new Array(numVertices);
+  for (var i = 0; i < numVertices; i++) {
+    pathLengths[i] = edges[startVertex][i];
+    if (edges[startVertex][i] != Infinity) {
+      predecessors[i] = startVertex;
+    }
+  }
+  pathLengths[startVertex] = 0;
+  for (var i = 0; i < numVertices - 1; i++) {
+    var closest = -1;
+    var closestDistance = Infinity;
+    for (var j = 0; j < numVertices; j++) {
+      if (!done[j] && pathLengths[j] < closestDistance) {
+        closestDistance = pathLengths[j];
+        closest = j;
+      }
+    }
+    done[closest] = true;
+    for (var j = 0; j < numVertices; j++) {
+      if (!done[j]) {
+        var possiblyCloserDistance = pathLengths[closest] + edges[closest][j];
+        if (possiblyCloserDistance < pathLengths[j]) {
+          pathLengths[j] = possiblyCloserDistance;
+          predecessors[j] = closest;
+        }
+      }
+    }
+  }
+  return { "startVertex": startVertex,
+           "pathLengths": pathLengths,
+           "predecessors": predecessors };
+}
+
+function constructPath(shortestPathInfo, endVertex) {
+  var path = [];
+  while (endVertex != shortestPathInfo.startVertex) {
+    path.unshift(endVertex);
+    endVertex = shortestPathInfo.predecessors[endVertex];
+  }
+  return path;
+}
