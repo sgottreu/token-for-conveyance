f6919e020c14229e12661a7fe7335623066c4e2d
Scott Gottreu <sgottreu@gmail.com>
2013-06-23 22:50:20 -0500

    Added new shortest route function

--- app/models/Route.js
+++ app/models/Route.js
@@ -39,3 +39,132 @@ routes.push([Infinity,Infinity,6,Infinity,Infinity,Infinity,Infinity,4,Infinity,
 
 
 exports.getRoutes = function() { return routes; }
+
+
+
+
+exports.getShortestRoute = function(graph, source, target, cities) { // graph = getRoutes = array(), source = index of originating city
+    
+    var dist = new Array();
+    var previous = new Array();
+    var route;
+    
+    for(v=0;v<graph.length;v++) {
+        dist[v] = Infinity;
+        previous[v] = undefined;
+    }   
+
+    dist[String(Number(source))] = 0 ;  
+    
+    console.log('source',source);
+    
+    var q = graph;
+    var vertex;
+    
+    console.log('Setting dist variable.');
+    console.log(dist);
+    
+    while(q.length > 0)
+    {
+        u = Infinity;
+        for(t=0;t<dist.length;t++)
+        {
+            //console.log(q[String(Number(t))]);
+            if(dist[t] < u && q[t] !== undefined) 
+            {
+                u = t;
+            }
+        }
+        console.log('u',u);
+        
+        vertex = q[u];
+        
+        //console.log(dist);
+        console.log('vertex',u);
+        console.log('Min distance: '+dist[u]);
+        console.log(cities[u+1]);
+        
+        q[u] = undefined;
+                
+        if(u == target) {
+            route = getSequence(previous, target);
+            
+            console.log(route);
+            break;
+        }
+        
+        if(dist[u] === Infinity)
+        {
+            break;
+        }
+console.log(previous);
+        for (v in vertex)
+        {
+            if (String(Number(v)) === v && vertex.hasOwnProperty(v)) 
+            {
+                if(vertex[v] !== Infinity && vertex[v] != 0)// && !inArray(v, previous))
+                {
+console.log('Looking at: '+cities[Number(v)+1]);
+console.log('vertex',v);
+console.log('with distance of: '+vertex[v]);
+
+
+                    var alt = dist[u] + vertex[v]; // vertex[v] = distance between v & u
+
+
+
+console.log('dist[u]',dist[u]);
+console.log('vertex[v]',vertex[v]);
+
+ console.log('alt', alt);                   
+ 
+ 
+                    if(alt < dist[v])
+                    {
+                        dist[Number(v)] = alt;
+                        previous[Number(v)] = u;
+                        //q[v][u] = 0;
+                    }
+                    
+                }
+            }
+            
+        }
+        
+        
+        
+    }    
+    
+    
+    
+    function getSequence(previous, target)
+    {
+        console.log('previous', previous);
+        console.log(target);
+        
+        var sequence = new Array();
+        var u = target
+        
+        while(previous[u] !== undefined)
+        {
+            sequence.push(u);
+            u = previous[u];
+        }
+        
+    }
+    
+    function inArray(needle, haystack) {
+        var key;
+        for(key in haystack) {
+            if (String(Number(key)) === key && vertex.hasOwnProperty(key)) 
+            {
+                if(key == needle) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    return { distance: dist, route: previous } ;
+}
